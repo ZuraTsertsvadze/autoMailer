bea53990530c261cf3e16942a0b399efb040a472
ZuraTsertsvadze <zuratsertsvadze1@gmail.com>
2024-09-07 10:39:35 +0400

    switch-to-graph-api

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 import chart from "./chart"
 import emailSender from "./emailSender";
 import processData from "./processData";
-import {saveAllUsers,saveClickedUsers} from "./processXl";
+import {saveAllUsers,saveClickedUsers,saveNotClickedUser} from "./processXl";
+import getFileFromDrive,{getLastUsers} from "./getFilesFromOneDrive";
+
 
 dotenv.config();
 
@@ -29,7 +31,7 @@ const data = {
   "კომპანიის დასახელება": "zazagroup",
   "თანამშრომელთა რაოდენობა კომპანიაში": "2",
   "ელექტრონული ფოსტა (აუცილებელია მიუთითოთ ვალიდური სამსახურებრივი ელ-ფოსტის მისამართი, წინააღმდეგ შემთხვევაში კვლევის მოკლე ანგარიში ვერ გამოგეგზავნებათ)":
-    "zazanozadze425@gmail.com",
+    "zazanozadze@gmail.com",
   "მოწოდებული ინფორმაცია დამუშავდება „პერსონალურ მონაცემთა დაცვის შესახებ“ საქართველოს კანონის შესაბამისად, წინამდებარე კითხვარიდან მიღებული ინფორმაციის ინტერპრეტაციისა და შედეგების ანგარიშის რესპონდენტი":
     "ვეთანხმები",
   "პროცესის საზღვრები":
@@ -52,16 +54,48 @@ const data = {
     "კომპანიის IT სისტემები ინტეგრირებულია. არსებული IT სისტემები პროცესების იდენტიფიცირების ანალიზის, მოდელირების და გაუმჯობესების უმნიშვნელოვანესი წყაროა.",
 };
 
+interface JsonData {
+  [key: string]: string | number;
+}
 
 
 app.get("/",async (req: Request, res: Response) => {
 
-  saveAllUsers(data)
+  const allUsers: JsonData[] = await getFileFromDrive(
+    "ბიზნეს პროცესების განვითარების ფაზების დიაგნოსტიკის კითხვარი.xlsx"
+  );
+
+ const scores=await getFileFromDrive('schema.xlsx')
+
+
+ const schemaScores=await getFileFromDrive('schemaScores.xlsx')
+
+// saveAllUsers(data)
 //  if(!req.body)return 
 //   const data:Data|undefined=await processData(req.body)
-//   chart(data);
+const mad={
+  "name": "ზაზა",
+  "lastName": "ნოზაძე",
+  "email": "2",
+  "id": "2",
+  "matrix": {
+    "პროცესის საზღვრები": "2",
+    "პროცესების აღწერილობები": "2",
+    "თამანშრომლების ფუნქციები და როლები": "5",
+    "მომხმარებელზე ორიენტირებული ბიზნეს პროცესები": "2",
+    "პროცესების ინდიკატორები": "3",
+    "მმართველობის როლი": "5",
+    "გადაწყვეტილებების მიღების პრაქტიკები": "2",
+    "პროცესების გაუმჯობესების პრაქტიკები": "4",
+    "ინფორმაციული ტექნოლოგიები": "4"
+  }
+}
+
+
+// const gag:any=processData(data)
+//   chart(mad);
 //   emailSender(data).catch((e)=>console.log(e))
-  // console.log(req.body)
+// console.log(req.body)
   res.send("Express + TypeScript Server")
 });
 
@@ -72,20 +106,29 @@ app.get("/",async (req: Request, res: Response) => {
 app.get('/tracking', (req, res) => {
   const { id, email,company} = req.query;
 
-  const clickedObject:any={id:id,email:email,company:company}
+  function unixToJulian(unixTimestamp: number) {
+    const julianDateStart = 2440587.5;
+    const milisecndInDay = 86400000;
+    return julianDateStart + unixTimestamp / milisecndInDay;
+  }
+
+const twoWeeksBackInMiliseconds = Date.now()
+const julianDate=unixToJulian(twoWeeksBackInMiliseconds)
+const clickedObject:any={"id":id,"Email":email,"company":company,"Completion time":julianDate}
  
 
   const clickedJson=JSON.stringify(clickedObject)
   // console.log(clickedJson)
-
-  saveClickedUsers(data)
-  saveAllUsers(data)
+  saveNotClickedUser()
+  // saveClickedUsers(clickedObject)
+  // saveAllUsers(data)
   // Record the click event with email
   // clicks.push({ id, email, timestamp: new Date() });
   // console.log(clicks)
   // Redirect to the actual URL
-  const actualUrl = '';
-  res.redirect(actualUrl);
+  // const actualUrl = 'https://m.media-amazon.com/images/I/61zcSevCQ1L._AC_UF1000,1000_QL80_.jpg';
+  // res.redirect(actualUrl);
+  res.end()
 });
 
 
--- src/processXl.ts
+++ src/processXl.ts
@@ -6,87 +6,109 @@ interface JsonData {
   [key: string]: string | number;
 }
 
+export const fileChecker = (directory: string, name: string) => {
+  const filePath = path.join(__dirname, directory);
 
-export const fileChecker=(directory:string,name:string)=>{
-    const filePath = path.join(__dirname, directory);
+  if (!fs.existsSync(filePath)) {
+    const workbook = xlsx.utils.book_new();
 
-    if (!fs.existsSync(filePath)) {
-      const workbook = xlsx.utils.book_new();
-  
-      // Create a new worksheet (empty)
-      const worksheet = xlsx.utils.aoa_to_sheet([]); // aoa_to_sheet([]) creates an empty sheet
-  
-      // Append the empty worksheet to the workbook
-      xlsx.utils.book_append_sheet(workbook, worksheet, name);
-  
-      xlsx.writeFile(workbook, filePath);
- 
-    }
-}
+    // Create a new worksheet (empty)
+    const worksheet = xlsx.utils.aoa_to_sheet([]); // aoa_to_sheet([]) creates an empty sheet
 
+    // Append the empty worksheet to the workbook
+    xlsx.utils.book_append_sheet(workbook, worksheet, name);
 
-
-
-export const xlsxToJson = (directory:string) => {
-
-     const filePath=path.join(__dirname,directory );
-
-    const workbook = xlsx.readFile(filePath);
-
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-
-    // Convert the sheet to JSON, using the first row as headers
-    const jsonData = xlsx.utils.sheet_to_json(worksheet);
-
-    // Save the JSON data to a file
-    // fs.writeFileSync('output.json', JSON.stringify(jsonData, null, 2));
-    console.log(jsonData);
-
-    return jsonData;
+    xlsx.writeFile(workbook, filePath);
+  }
 };
 
-const saver = (savedJson:any,directory:string,name:string,data:any) => {
+export const xlsxToJson = (directory: string) => {
+  const filePath = path.join(__dirname, directory);
 
-  const newJson = [...savedJson, data];
+  const workbook = xlsx.readFile(filePath);
+
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+
+  // Convert the sheet to JSON, using the first row as headers
+  const jsonData: JsonData[] = xlsx.utils.sheet_to_json(worksheet);
+
+  // Save the JSON data to a file
+  // fs.writeFileSync('output.json', JSON.stringify(jsonData, null, 2));
+
+  return jsonData;
+};
+
+export const saver = (
+  savedJson: any,
+  directory: string,
+  name: string,
+  data: any
+) => {
+  const newJson = [savedJson, data].flat();
 
   const ws = xlsx.utils.json_to_sheet(newJson);
   const wb = xlsx.utils.book_new();
   xlsx.utils.book_append_sheet(wb, ws, name);
 
-    const filePath = path.join(__dirname, directory);
-    xlsx.writeFile(wb, filePath);
-
-
+  const filePath = path.join(__dirname, directory);
+  xlsx.writeFile(wb, filePath);
 };
 
-
-export const saveAllUsers=(data:any)=>{
-  fileChecker("../assets/allUsers.xlsx","allUsers")
+export const saveAllUsers = (data: any) => {
+  fileChecker("../assets/allUsers.xlsx", "allUsers");
   const savedJson = xlsxToJson("../assets/allUsers.xlsx");
-  saver(savedJson,"../assets/allUsers.xlsx","allUsers",data)
+  saver(savedJson, "../assets/allUsers.xlsx", "allUsers", data);
+};
 
-}
-
-
-export const saveClickedUsers=(data:any)=>{
-
-  fileChecker("../assets/clickedUsers.xlsx","clickedUsers")
+export const saveClickedUsers = (data: any) => {
+  fileChecker("../assets/clickedUsers.xlsx", "clickedUsers");
   const savedJson = xlsxToJson("../assets/clickedUsers.xlsx");
-  saver(savedJson,"../assets/clickedUsers.xlsx","clickedUsers",data)
+  saver(savedJson, "../assets/clickedUsers.xlsx", "clickedUsers", data);
+};
 
+export const saveNotClickedUser = () => {
+  function unixToJulian(unixTimestamp: number) {
+    const julianDateStart = 2440587.5;
+    const milisecndInDay = 86400000;
+    return julianDateStart + unixTimestamp / milisecndInDay;
+  }
 
-}
+  const allUsersServerJson = xlsxToJson("../assets/allUsersServer.xlsx");
+  const twoWeeksBackInMiliseconds = Date.now() - 1209600000;
 
+  const usersOfTwoWeeksServer = allUsersServerJson.map((user) => {
+    // console.log( user?.["Completion time"] )
+    if (
+      (user?.["Completion time"] as number) <=
+      unixToJulian(twoWeeksBackInMiliseconds)
+    ) {
+      //shesacvleni metia
+      return user;
+    }
+  });
 
-export const saveNotClickedUser=()=>{
+  const clickedUsersJson = xlsxToJson("../assets/clickedUsers.xlsx");
 
-}
+  const usersOfTwoWeeksClicked = clickedUsersJson.map((user) => {
+    // console.log( user?.["Completion time"] )
+    if (
+      (user?.["Completion time"] as number) >=
+      unixToJulian(twoWeeksBackInMiliseconds)
+    ) {
+      // console.log(user?.["Completion time"],"fromClicked")
+      return user;
+    }
+  });
 
+  const clickedUsers = usersOfTwoWeeksClicked.map((clickedUser) => clickedUser?.Email);
+  const notClickedUsers = usersOfTwoWeeksServer.flatMap((user) => {
+    if (!clickedUsers.includes(user?.Email)) {
+      return [user?.Email,user?.['კომპანიის დასახელება']];
+    }
 
+    return [];
+  });
 
-
-
-
-
-
+  console.log(notClickedUsers);
+};
